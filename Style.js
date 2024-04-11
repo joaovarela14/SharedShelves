53552ba8e526dce3b800f4c4662dbe8c131b782e
João Varela <joaovarela@ua.pt>
2024-04-11 13:01:35 +0100

    working on profile

---
+++ Style.js
@@ -0,0 +1,23 @@
+import { Button, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    button: {
+        backgroundColor: 'blue',
+        padding: 7,
+        borderRadius: 3,
+      },
+      buttonText: {
+        fontSize: 15,
+        color: 'white',
+      },
+
+});
+
+export default styles;
--- navigation/screens/ProfileScreen.js
+++ navigation/screens/ProfileScreen.js
@@ -1,13 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Button} from 'react-native';
+import styles from '../../Style';
+import { MaterialIcons } from '@expo/vector-icons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 export default function ProfileScreen({ navigation }) {
   return (
-        <View style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start', paddingHorizontal: 20, paddingTop: 20 }}>
-      <Text style={{ fontSize: 25, fontWeight: 'bold' }}>Profile Screen</Text>
-    
+      <View style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start', paddingHorizontal: 20, paddingTop: 30 }}>
+        
+      
+      
+      <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.button}>
+        <Text style={styles.buttonText}>Go to Home Screen</Text>
+      </TouchableOpacity>
+
+
+
     </View>
+    
 
   );
 }
