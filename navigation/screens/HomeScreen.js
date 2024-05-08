9ba9de6cddbd31b52153dc0944027f728c51d0de
Bruhlin <joaomorais30478@gmail.com>
2024-05-08 15:48:57 +0100

    feed (must change styling and add wishlist)

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,10 +1,83 @@
-import * as React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, Image, StyleSheet } from 'react-native';
 
-export default function HomeScreen({ navigation }) {
+import actionsData from '../../actions.json'
+
+export default function Feed() {
+  const [actions, setActions] = useState([]);
+
+  useEffect(() => {
+    setActions(actionsData);
+  }, []);
+
+  const renderAction = ({ item }) => {
+
+    if (item.type === "friendAddToWishlist") {
+        return (
+            <View style={styles.action}>
+                <View style={styles.actionContent}>
+                    <Text>{'Your friend ' + item.name + ' added ' + item.book + ' to the wishlist.'}</Text>
+                </View>
+            </View>
+        );
+    } else if (item.type === 'newPromo') {
+        return (
+            <View style={styles.action}>
+                <View style={styles.actionContent}>
+                    <Text>{'⚠️ PROMO ⚠️ ' + item.date + ' ' + item.location + ' ' + item.discount + " on " + item.bookType + " books."}</Text>
+                </View>
+            </View>
+        );
+    } else if (item.type === 'newBookAvailable') {
+        return (
+             <View style={styles.action}>
+                 <View style={styles.actionContent}>
+                    <Text>{'The book ' + item.book + ' is available for ' + item.numPoints + ' points at ' + item.location + "."}</Text>
+                 </View>
+             </View>
+        );
+    }
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text style={{ fontSize: 25, fontWeight: 'bold' }}> HOME</Text> 
+    <View style={styles.container}>
+      <Text style={styles.logo}>{'Shared Shelves'}</Text>
+      <FlatList
+        data={actions}
+        renderItem={renderAction}
+        keyExtractor={action => action.id}
+        contentContainerStyle={styles.actionList}
+        style={{ paddingTop: 50 }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+
+    backgroundColor: '#f5f5f5',
+  },
+  actionList: {
+    paddingHorizontal: 16,
+  },
+  action: {
+    backgroundColor: '#fff',
+    padding: 16,
+    marginBottom: 12,
+    borderRadius: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  actionContent: {
+    flex: 1,
+  },
+
+  logo: {
+    textAlign: 'center',
+    fontSize: 20,
+    paddingTop: 40
+  },
+
+});
